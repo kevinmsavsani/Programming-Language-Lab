fae0d9473f6416c55d93b0994f88b296a53fff89
kevinmsavsani <kevinmsavsani@gmail.com>
2019-09-27 01:08:36 +0530

    Prolog Task 1 Basic Solution

--- Prolog/1.pl
+++ Prolog/1.pl
@@ -1,24 +1,97 @@
-isOne(N) :- N=1.
-isTwo(N) :- N=2.
-isGreaterThanZero(N) :- N>=0.
-isLessThanSix(N) :- N>=0,N<7.
 
-
-decode(S1) :-
+decode(S1,Sum,C) :-
     string_chars(S1, L1),
-    decode_List(L1).
+    list_sum(L1,Sum,C).
+%    decode_List(L1).
 
-decode_head(L) :- write(L),atom_number(L,N),isLessThanSix(N).
+%decode_head(L) :- write(L),atom_number(L,N),isLessThanSix(N).
+%
+%decode_List([]).
+%decode_List([H|T]) :- decode_head(H),decode_List(T).
 
-decode_List([]).
-decode_List([H|T]) :- decode_head(H),decode_List(T).
+isNextGreaterThanSix([]) :- !.
+isNextGreaterThanSix([X|Xs]) :- atom_number(X,N), N>6.
+isNextLessThanSeven([X|Xs]) :- atom_number(X,N), N<7.
 
-fib(0, 1) :- !.
-fib(1, 1) :- !.
-fib(N, F) :-
-        N > 1,
-        N1 is N-1,
-        N2 is N-2,
-        fib(N1, F1),
-        fib(N2, F2),
-        F is F1+F2.
+list_sum([], 1, 0).
+list_sum([X|Xs], Sum, Ci) :-
+    list_sum(Xs, S0 , C0),
+    atom_number(X,N),
+    N=0,
+    Sum is 0,
+    Ci is S0.
+list_sum([X|Xs], Sum, Ci) :-
+    list_sum(Xs, S0, C0),
+    atom_number(X,N),
+    N=1,
+    Sum is S0 +C0,
+    Ci is S0.
+%    writeln(" "),
+%    write(X),
+%    write(" "),
+%    write(Xs),
+%    write(" "),
+%    write(Sum),
+%    write(" "),
+%    write(S0),
+%    write(" "),
+%    write(Ci),
+%    write(" "),
+%    write(C0).
+list_sum([X|Xs], Sum, Ci) :-
+    list_sum(Xs, S0, C0),
+    atom_number(X,N),
+    N=2,
+    isNextLessThanSeven(Xs),
+    Sum is S0 + C0,
+    Ci is S0.
+%    writeln(" "),
+%    write(X),
+%    write(" "),
+%    write(Xs),
+%    write(" "),
+%    write(Sum),
+%    write(" "),
+%    write(S0),
+%    write(" "),
+%    write(Ci),
+%    write(" "),
+%    write(C0).
+list_sum([X|Xs], Sum, Ci) :-
+    list_sum(Xs, S0, C0),
+    atom_number(X,N),
+    N =2,
+    isNextGreaterThanSix(Xs),
+    Sum is S0,
+    Ci is S0.
+%    writeln(" "),
+%    write(X),
+%    write(" "),
+%    write(Xs),
+%    write(" "),
+%    write(Sum),
+%    write(" "),
+%    write(S0),
+%    write(" "),
+%    write(Ci),
+%    write(" "),
+%    write(C0).
+list_sum([X|Xs], Sum, Ci) :-
+    list_sum(Xs, S0, C0),
+    atom_number(X,N),
+    N>2,
+    Sum is S0,
+    Ci is S0.
+%    writeln(" "),
+%    write(X),
+%    write(" "),
+%    write(Xs),
+%    write(" "),
+%    write(Sum),
+%    write(" "),
+%    write(S0),
+%    write(" "),
+%    write(Ci),
+%    write(" "),
+%    write(C0).
+
