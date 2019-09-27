6d08e9e500a7ed1d4ff7c21a00faccf9fe18d5f3
kevinmsavsani <kevinmsavsani@gmail.com>
2019-09-27 19:26:40 +0530

    Prolog Task 3 basic solution

--- Prolog/3.pl
+++ Prolog/3.pl
@@ -1,4 +1,131 @@
 
+edge(g1,g5,4).
+edge(g2,g5,6).
+edge(g3,g5,8).
+edge(g4,g5,9).
+edge(g1,g6,10).
+edge(g2,g6,9).
+edge(g3,g6,3).
+edge(g4,g6,5).
+edge(g5,g7,3).
+edge(g5,g10,4).
+edge(g5,g11,6).
+edge(g5,g12,7).
+edge(g5,g6,7).
+edge(g5,g8,9).
+edge(g6,g8,2).
+edge(g6,g12,3).
+edge(g6,g11,5).
+edge(g6,g10,9).
+edge(g6,g7,10).
+edge(g7,g10,2).
+edge(g7,g11,5).
+edge(g7,g12,7).
+edge(g7,g8,10).
+edge(g8,g9,3).
+edge(g8,g12,3).
+edge(g8,g11,4).
+edge(g8,g10,8).
+edge(g10,g15,5).
+edge(g10,g11,2).
+edge(g10,g12,5).
+edge(g11,g15,4).
+edge(g11,g13,5).
+edge(g11,g12,4).
+edge(g12,g13,7).
+edge(g12,g14,8).
+edge(g15,g13,3).
+edge(g13,g14,4).
+edge(g14,g17,5).
+edge(g14,g18,4).
+edge(g17,g18,8).
+
+edge(g15,g16,5).
+start(g1).
+start(g2).
+start(g3).
+start(g4).
+end(g16).
+end(g17).
+
+
+% if he specify from which gate to which gate
+routing(FromCity, ToCity, [FromCity, ToCity]) :-
+  edge(FromCity, ToCity, _).
+
+routing(FromCity, ToCity, [FromCity|Connections]) :-
+  edge(FromCity, ToConnection, _),
+  routing(ToConnection, ToCity, Connections).
+
+% find all edge from all gate
+all_path(L) :-
+    all_path(_,_,L).
+all_path(Start,End,L) :-
+    start(Start),
+    end(End),
+    routing(Start, End, L).
+
+%path(X, Y, N, [X-Y]) :- edge(X, Y, N).
+%path(X, Z, N, [X-Y|T]) :-
+%    edge(X, Y, N0),
+%    path(Y, Z, N1, T),
+%    N is N0 + N1.
+
+path(X, Y, N, Path) :- path(X, Y, N, [], Path).
+
+path(X, Y, N, Seen, [X]) :-
+    \+ memberchk(X, Seen),
+    edge(X, Y, N).
+path(X, Z, N, Seen, [X|T]) :-
+    \+ memberchk(X, Seen),
+    edge(X, Y, N0),
+    path(Y, Z, N1, [X|Seen], T),
+    \+ memberchk(X, T),
+    N is N0 + N1.
+
+memberchk(X, L) :- once(member(X, L)).
+member(X, [X|_]).
+member(X, [_|Xs]) :- member(X, Xs).
+
+uniq_shortest_path(X, Y, MinCost, Path) :-
+    path(X, Y, MinCost, Path),
+    \+ (path(X, Y, LowerCost, OtherPath),
+        OtherPath \= Path,
+        LowerCost =< MinCost).
+
+% Prolog Conventions
+
+
+find_shortest_path([], _, MinPath, MinPath).
+
+find_shortest_path([Path|Paths], MinLen, _, Output) :-
+    length(Path, N),
+    N < MinLen,
+    find_shortest_path(Paths, N, Path, Output).
+
+find_shortest_path([Path|Paths], MinLen, MinPath, Output) :-
+    length(Path, N),
+    N >= MinLen,
+    find_shortest_path(Paths, MinLen, MinPath, Output).
+
+optimal(L) :-
+    optimal(_, _, L, _, _).
+
+optimal(Start, End, L, Count, PathLength) :-
+    start(Start),
+    end(End),
+    findall(Path, routing(Start, End, Path), Paths),
+    find_shortest_path(Paths, 1000 , [], L),
+    length(L, PathLength),
+    length(Paths, Count).
 
 
 
+
+% to get min weight path
+%path(S,D,TDist):-
+%    edge(S,D,TDist).
+%path(S,D,TDist):-
+%    edge(S,X,TD1), path(X,D,TD2), TDist=TD1+TD2.
+%
+%aggregate(min(D), path(g1,g17,D), D).
