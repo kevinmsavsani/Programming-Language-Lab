37365b2e040bb4a0a45dfb3d8da62d1425e07fa7
kevinmsavsani <kevinmsavsani@gmail.com>
2019-10-25 22:52:34 +0530

    Prolog Task 1 final code update

--- Prolog/1.pl
+++ Prolog/1.pl
@@ -1,99 +1,20 @@
 
+decode(String,Sum) :-
+    string_chars(String, List),
+    list_sum(List,Sum,_).
 
-decode(S1,Sum) :-
-    string_chars(S1, L1),
-    list_sum(L1,Sum,C).
-%    decode_List(L1).
-
-%decode_head(L) :- write(L),atom_number(L,N),isLessThanSix(N).
-%
-%decode_List([]).
-%decode_List([H|T]) :- decode_head(H),decode_List(T).
-
-isNextGreaterThanSix([]) :- !.
-isNextLessThanSeven([]) :- !.
-isNextGreaterThanSix([X|Xs]) :- atom_number(X,N), N>6.
-isNextLessThanSeven([X|Xs]) :- atom_number(X,N), N<7.
+isNextLessThanSeven([X|_]) :- atom_number(X,N), N<7.
 
 list_sum([], 1, 0).
 list_sum([X|Xs], Sum, Ci) :-
-    atom_number(X,N),
-    N=0,
     list_sum(Xs, S0 , C0),
-    Sum is 0,
-    Ci is S0.
-list_sum([X|Xs], Sum, Ci) :-
     atom_number(X,N),
-    N=1,
-    list_sum(Xs, S0, C0),
-    Sum is S0 +C0,
-    Ci is S0.
-%    writeln(" "),
-%    write(X),
-%    write(" "),
-%    write(Xs),
-%    write(" "),
-%    write(Sum),
-%    write(" "),
-%    write(S0),
-%    write(" "),
-%    write(Ci),
-%    write(" "),
-%    write(C0).
-list_sum([X|Xs], Sum, Ci) :-
-    atom_number(X,N),
-    N=2,
-    isNextLessThanSeven(Xs),
-    list_sum(Xs, S0, C0),
-    Sum is S0 + C0,
-    Ci is S0.
-%    writeln(" "),
-%    write(X),
-%    write(" "),
-%    write(Xs),
-%    write(" "),
-%    write(Sum),
-%    write(" "),
-%    write(S0),
-%    write(" "),
-%    write(Ci),
-%    write(" "),
-%    write(C0).
-list_sum([X|Xs], Sum, Ci) :-
-    atom_number(X,N),
-    N =2,
-    isNextGreaterThanSix(Xs),
-    list_sum(Xs, S0, C0),
-    Sum is S0,
-    Ci is S0.
-%    writeln(" "),
-%    write(X),
-%    write(" "),
-%    write(Xs),
-%    write(" "),
-%    write(Sum),
-%    write(" "),
-%    write(S0),
-%    write(" "),
-%    write(Ci),
-%    write(" "),
-%    write(C0).
-list_sum([X|Xs], Sum, Ci) :-
-    atom_number(X,N),
-    N>2,
-    list_sum(Xs, S0, C0),
-    Sum is S0,
-    Ci is S0.
-%    writeln(" "),
-%    write(X),
-%    write(" "),
-%    write(Xs),
-%    write(" "),
-%    write(Sum),
-%    write(" "),
-%    write(S0),
-%    write(" "),
-%    write(Ci),
-%    write(" "),
-%    write(C0).
-
+    (   N = 0 ->
+        Sum is 0,
+        Ci is S0
+    ;   (N =:= 1 ; N =:= 2, isNextLessThanSeven(Xs) )->
+        Sum is S0 +C0,
+        Ci is S0
+    ;   Sum is S0 ,
+        Ci is S0
+    ).
