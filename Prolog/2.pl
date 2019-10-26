2b5aa6d2d9fc3b060213bba6649329a2840f0663
Savinay Parihar <savinay1228@hotmail.com>
2019-10-26 20:06:16 +0530

    Commented 2.pl

--- Prolog/2.pl
+++ Prolog/2.pl
@@ -1,3 +1,5 @@
+
+% facts for calorie for each starter
 starter("Corn Tikki",30).
 starter("Tomato Soup", 20).
 starter("Chilli Paneer", 40).
@@ -5,16 +7,19 @@ starter("Crispy chicken", 40).
 starter("Papdi Chaat", 20).
 starter("Cold Drink", 20).
 
+% facts for calorie for each maindish item
 mainDish("Kadhai Paneer with Butter / Plain Naan" ,50).
 mainDish("Veg Korma with Butter / Plain Naan" ,40).
 mainDish("Murgh Lababdar with Butter / Plain Naan"  ,50).
 mainDish("Veg Dum Biryani with Dal Tadka" ,50).
 mainDish("Steam Rice with Dal Tadka" ,40).
 
-dessert("Ice-cream" ,20).
-dessert("Malai Sandwich" ,30).
-dessert("Rasmalai" ,10).
+% facts for calorie for each desert item
+desert("Ice-cream" ,20).
+desert("Malai Sandwich" ,30).
+desert("Rasmalai" ,10).
 
+% facts to check validity of menu    format - <status,x,y,z> status-(hungry,notsohungry, diet)   x-starter, y-maindish, z- desert
 menu(hungry,1,1,1).
 menu(notSoHungry,1,1,0).
 menu(notSoHungry,0,1,1).
@@ -22,29 +27,40 @@ menu(diet,0,0,1).
 menu(diet,0,1,0).
 menu(diet,1,0,0).
 
+
+writer1([]) :- writeln("").
+writer1([[A,_]|T]) :-  write(" , "),write(A),writer1(T).
+writer([[A,_]|T]) :- write("Items: "), write(A),writer1(T).
+
+% get all subset items of given list with condition calorie value less than given max value
 subset([], [],0,_).
 subset(L, [_|T],S,M) :- subset(L, T , S1,M), S = S1.
 subset([[H,A]|T1], [[H,A]|T2],S,M) :- subset(T1, T2, S1,M), S = S1 + A,S=<M.
 subset([[]|T1], [[_,A]|T2],S,M) :- subset(T1, T2, S1,M), S = S1 + A,S>M,S =S1.
 
-
-find_item(hungry,1,1,1) :- starter(A,_),mainDish(B,_),dessert(C,_),write("Items: "),write(A),write(" , "),write(B),write(" , "),writeln(C).
+% rules to get valid items list for perticular combination of status and menu
+% when hungry, get all items and ignore their calorie values
+find_item(hungry,1,1,1) :- starter(A,_),mainDish(B,_),desert(C,_),write("Items: "),write(A),write(" , "),write(B),write(" , "),writeln(C).
+%  item can be from maindish and starter or maindish and desert but total calorie in each pair must be less than equal to 80
 find_item(notSoHungry,1,1,0) :- mainDish(B,C), starter(A,M), M+C =< 80,write("Items: "), write(A),write(" , "),writeln(B).
-find_item(notSoHungry,0,1,1) :- mainDish(B,C), dessert(A,M), M+C =< 80,write("Items: "), write(A),write(" , "), writeln(B).
+find_item(notSoHungry,0,1,1) :- mainDish(B,C), desert(A,M), M+C =< 80,write("Items: "), write(A),write(" , "), writeln(B).
+
+% when on diet take all subset of any one (starter,maindish, desert) where calorie less than equal to 40
 find_item(diet,0,0,1,R) :- subset(X,R,M,40), M > 0,writer(X).
 find_item(diet,0,1,0,R) :- subset(X,R,M,40), M > 0,writer(X).
 find_item(diet,1,0,0,R) :- subset(X,R,M,40), M > 0,writer(X).
 
+% get all possible output for all values of arguments
 find_items(hungry,1,1,1) :- findall(_,find_item(hungry,1,1,1),_).
 find_items(notSoHungry,1,1,0) :- findall(_,find_item(notSoHungry,1,1,0),_).
 find_items(notSoHungry,0,1,1) :- findall(_,find_item(notSoHungry,0,1,1),_).
-find_items(diet,0,0,1) :- findall([A,Y],(dessert(A,Y),Y=<40),R), findall(_,find_item(diet,0,0,1,R),_).
+find_items(diet,0,0,1) :- findall([A,Y],(desert(A,Y),Y=<40),R), findall(_,find_item(diet,0,0,1,R),_).
 find_items(diet,0,1,0) :- findall([A,Y],(mainDish(A,Y),Y=<40),R), findall(_,find_item(diet,0,1,0,R),_).
 find_items(diet,1,0,0) :- findall([A,Y],(starter(A,Y),Y=<40),R), findall(_,find_item(diet,1,0,0,R),_).
 
-writer1([]) :- writeln("").
-writer1([[A,_]|T]) :-  write(" , "),write(A),writer1(T).
-writer([[A,_]|T]) :- write("Items: "), write(A),writer1(T).
+
+
+% code section to print output with repeatation of same dish allowed logic same as above with one speacial condition commented below
 
 adds([],0).
 adds([_,A],A).
@@ -55,6 +71,7 @@ fill([X|Xs], X, N,M,S) :- succ(N0, N), fill(Xs, X, N0,M,S1),adds(X,A),S is S1 +A
 append([],L,L).
 append([H|T],L2,[H|L3])  :-  append(T,L2,L3).
 
+% for allowing repeatation here we are considring all subsets with head being fixed
 subset_repe([], [],0,_).
 subset_repe(L, [_|T],S,M) :- subset_repe(L, T , S1,M), S = S1.
 subset_repe(R, [[H,A]|T2],S,M) :- subset_repe(T1, T2, S1,M-A),  N is div((M-S1),A), N>0, fill(R2,[H,A],N,N,S4),S is S4+S1,append(R2,T1,R).
@@ -63,7 +80,7 @@ find_item_repe(diet,0,0,1,R) :- subset_repe(X,R,M,40), M > 0,writer(X).
 find_item_repe(diet,0,1,0,R) :- subset_repe(X,R,M,40), M > 0,writer(X).
 find_item_repe(diet,1,0,0,R) :- subset_repe(X,R,M,40), M > 0,writer(X).
 
-find_items_repe(diet,0,0,1) :- findall([A,Y],(dessert(A,Y),Y=<40),R), findall(_,find_item_repe(diet,0,0,1,R),_).
+find_items_repe(diet,0,0,1) :- findall([A,Y],(desert(A,Y),Y=<40),R), findall(_,find_item_repe(diet,0,0,1,R),_).
 find_items_repe(diet,0,1,0) :- findall([A,Y],(mainDish(A,Y),Y=<40),R), findall(_,find_item_repe(diet,0,1,0,R),_).
 find_items_repe(diet,1,0,0) :- findall([A,Y],(starter(A,Y),Y=<40),R), findall(_,find_item_repe(diet,1,0,0,R),_).
 
@@ -74,4 +91,4 @@ find_item_complex(notSoHungry,1,1,0,K,R) :- subset(L,K,C,80),C>0, subset(X,R,M,8
 find_item_complex(notSoHungry,0,1,1,K,R) :- subset(L,K,C,80),C>0, subset(X,R,M,80-C), M>0 , write("Items: "), writer2(L), write(" , "),writer2(X),writeln("").
 
 find_items_complex(notSoHungry,1,1,0) :- findall([I,J],mainDish(I,J),K), findall([A,Y],starter(A,Y),R),findall(_,find_item_complex(notSoHungry,0,1,1,K,R),_).
-find_items_complex(notSoHungry,0,1,1) :- findall([I,J],mainDish(I,J),K), findall([A,Y],dessert(A,Y),R),findall(_,find_item_complex(notSoHungry,0,1,1,K,R),_).
+find_items_complex(notSoHungry,0,1,1) :- findall([I,J],mainDish(I,J),K), findall([A,Y],desert(A,Y),R),findall(_,find_item_complex(notSoHungry,0,1,1,K,R),_).
